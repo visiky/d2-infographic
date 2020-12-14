3732b4f9c7617d521e8bcbb2147e2dcb426788f5
xinming <xinming.lxj@antfin.com>
2020-12-14 14:40:46 +0800

    chore: 将 g2plot 设置为 external

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,28 @@
-// import { uglify } from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import size from 'rollup-plugin-size';
-import commonjs from '@rollup/plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs';
 
-module.exports = [{
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/d2-infographic.min.js',
-    name: 'D2Infographic',
-    format: 'umd',
-    sourcemap: false,
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/d2-infographic.min.js',
+      name: 'D2Infographic',
+      format: 'umd',
+      sourcemap: false,
+      globals: {
+        '@antv/g2plot': 'G2Plot',
+      },
+    },
+    external: ['@antv/g2plot'],
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs(),
+      uglify(),
+      size({}),
+    ],
   },
-  plugins: [
-    resolve(),
-    typescript(),
-    commonjs(),
-    // todo https://www.jianshu.com/p/4c66db0d7a7d
-    // uglify(),
-    size({}),
-  ],
-}];
+];
